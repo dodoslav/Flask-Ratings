3ae58db9f2e41d4c775eba06e6c811bb91f1a26b
Jozef Resetar <jozefresetar@gmail.com>
2018-02-23 12:50:42 +0100

    added credentials_example.py

---
+++ credentials_example.py
@@ -0,0 +1,5 @@
+class Credentials(object):
+    google_id = 'google id'
+    google_secret = 'google secret'
+    secret_token = 'just secret and very long random string'
+    secret_key = 'again just secret and long random string'
